928e72ea33441c3e4802e6ebdb73e4d4cbe29812
benhayehudi <bengreenberg@gmail.com>
2019-01-19 19:59:52 +0200

    updated rails to 5.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :verify_user!, unless: :devise_controller?
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
 
